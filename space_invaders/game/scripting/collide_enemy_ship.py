6715d677e9ca082aa04ad85ea17ecdbf2aae1f22
Carter Thompson <carthomp88@gmail.com>
2022-03-29 21:10:31 -0500

    Bullet now returns to ship on collision

--- space_invaders/game/scripting/collide_enemy_ship.py
+++ space_invaders/game/scripting/collide_enemy_ship.py
@@ -1,6 +1,10 @@
 from constants import *
 from game.casting.sound import Sound
 from game.scripting.action import Action
+from game.casting.point import Point
+from game.casting.body import Body
+from game.casting.image import Image
+from game.casting.bullet import Bullet
 
 # Formerly CollideBrickAction
 
@@ -10,6 +14,20 @@ class CollideEnemyShip(Action):
     def __init__(self, physics_service, audio_service):
         self._physics_service = physics_service
         self._audio_service = audio_service
+    
+    def remake_bullet(self, cast):
+        ship = cast.get_first_actor(RACKET_GROUP)
+        ship_body = ship.get_body()
+        ship_position = ship_body.get_position()
+        x = ship_position.get_x()
+        y = SCREEN_HEIGHT - RACKET_HEIGHT - BULLET_HEIGHT  
+        position = Point(x, y)
+        size = Point(BULLET_WIDTH, BULLET_HEIGHT)
+        velocity = Point(0, 0)
+        body = Body(position, size, velocity)
+        image = Image(BULLET_IMAGE)
+        bullet = Bullet(body, image, True)
+        cast.add_actor(BULLET_GROUP, bullet)
 
     def execute(self, cast, script, callback):
         ball = cast.get_first_actor(BULLET_GROUP)
@@ -26,4 +44,6 @@ class CollideEnemyShip(Action):
                 points = brick.get_points()
                 stats.add_points(points)
                 cast.remove_actor(BRICK_GROUP, brick)
+                self.remake_bullet(cast)
                 cast.remove_actor(BULLET_GROUP, ball)
+                
