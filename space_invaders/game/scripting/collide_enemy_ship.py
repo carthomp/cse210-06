8fcbf47d22e0ffc8f32313632898f654d81ec4eb
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-03-29 23:13:45 -0300

    collide enemy ship  chang names

--- space_invaders/game/scripting/collide_enemy_ship.py
+++ space_invaders/game/scripting/collide_enemy_ship.py
@@ -12,18 +12,18 @@ class CollideEnemyShip(Action):
         self._audio_service = audio_service
 
     def execute(self, cast, script, callback):
-        ball = cast.get_first_actor(BULLET_GROUP)
+        bullet = cast.get_first_actor(BULLET_GROUP)
         bricks = cast.get_actors(BRICK_GROUP)
         stats = cast.get_first_actor(STATS_GROUP)
 
         for brick in bricks:
-            ball_body = ball.get_body()
+            bullet_body = bullet.get_body()
             brick_body = brick.get_body()
 
-            if self._physics_service.has_collided(ball_body, brick_body):
+            if self._physics_service.has_collided(bullet_body, brick_body):
                 sound = Sound(BOUNCE_SOUND)
                 self._audio_service.play_sound(sound)
                 points = brick.get_points()
                 stats.add_points(points)
                 cast.remove_actor(BRICK_GROUP, brick)
-                cast.remove_actor(BULLET_GROUP, ball)
+                cast.remove_actor(BULLET_GROUP, bullet)
