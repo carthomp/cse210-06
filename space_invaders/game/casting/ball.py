f5ad2adce8733eff25a99069da0db33de9cdb5ec
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-03-29 22:26:25 -0300

    ball and bullet small changes

--- space_invaders/game/casting/ball.py
+++ space_invaders/game/casting/ball.py
@@ -6,8 +6,8 @@ from game.casting.point import Point
 
 class Ball(Actor):
     """A solid, spherical object that is bounced around in the game."""
-    
-    def __init__(self, body, image, debug = False):
+
+    def __init__(self, body, image, debug=False):
         """Constructs a new Ball.
 
         Args:
@@ -19,27 +19,9 @@ class Ball(Actor):
         self._body = body
         self._image = image
 
-    def bounce_x(self):
-        """Bounces the ball in the x direction."""
-        velocity = self._body.get_velocity()
-        rn = random.uniform(0.9, 1.1)
-        vx = velocity.get_x() * rn * -1
-        vy = velocity.get_y()
-        velocity = Point(vx, vy)
-        self._body.set_velocity(velocity)
-
-    def bounce_y(self):
-        """Bounces the ball in the y direction."""
-        velocity = self._body.get_velocity()
-        rn = random.uniform(0.9, 1.1)
-        vx = velocity.get_x()
-        vy = velocity.get_y() * rn * -1 
-        velocity = Point(vx, vy)
-        self._body.set_velocity(velocity)
-
     def get_body(self):
         """Gets the ball's body.
-        
+
         Returns:
             An instance of Body.
         """
@@ -47,12 +29,12 @@ class Ball(Actor):
 
     def get_image(self):
         """Gets the ball's image.
-        
+
         Returns:
             An instance of Image.
         """
         return self._image
-        
+
     def release(self):
         """Release the ball in a random direction."""
         rn = random.uniform(0.9, 1.1)
